7dffd1324719f0f77ed0258a368322078e01aeb9
WizzerOmega <ol.lefrancois08@gmail.com>
2015-09-02 08:47:33 +0200

    session

--- www/js/services.js
+++ www/js/services.js
@@ -150,3 +150,8 @@ angular.module('smartstock.services', [])
             }
         };
     });
+
+angular.module('starter.services', ['ngResource'])
+    .factory('Session', function ($resource) {
+        return $resource('http://localhost:5000/sessions/:sessionId');
+    });
